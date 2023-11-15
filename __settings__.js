4db1ad39aaca6eeb52fb718941f735362e278ab9
cindy0705 <57369087+cindy0705@users.noreply.github.com>
2023-11-15 10:33:48 -0500

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,24 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1172537.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 49295753, 49041518, 50540733, 49138681, 49776020, 49867563, 49867896, 49867897, 49868997, 49876175, 49879545, 49879831, 49879891, 50505578 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/49042034/1/basis.wasm.js', 'wasmUrl' : 'files/assets/49042035/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/49042033/1/basis.js', 'preload' : false},
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/49100638/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/49100639/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/49100637/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/49957636/1/basis.wasm.js', 'wasmUrl' : 'files/assets/49957637/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/49957635/1/basis.js', 'preload' : false},
+];
